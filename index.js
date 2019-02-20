b5054e6130b7cb02fec5d1caaa615757693d1889
Charlotte Shih <shih.charlotte@gmail.com>
2019-02-19 21:01:34 -0800

    finished assignment 2, added styling

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
+'use strict';
+
 function handleSubmitButton() {
-    $('form').submit(function(event) {
+    $('.dog-generator').submit(function(event) {
         event.preventDefault();
         let selection = $('.1-50').val();
-        console.log('submit button pressed');
-        console.log(selection);
-        getDogImages(selection);
+        logDogImages(selection);
+        generateDogImages(selection);
     });
 }
 
 function generateDropDownMenu() {
-    for (i = 4; i <= 50; i++) {
+    for (let i = 4; i <= 50; i++) {
         $('.1-50').append(
             $('<option></option>').val(i).html(i)
         )
-    }
+    };
 }
 
-function getDogImages(selection) {
-    fetch('https://dog.ceo/api/breeds/image/random/'+selection)
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+function logDogImages(selection) {
+    fetch('https://dog.ceo/api/breeds/image/random/'+ selection)
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson));
+}
+
+function generateDogImages(selection) {
+    fetch('https://dog.ceo/api/breeds/image/random/'+ selection)
+        .then(response => response.json())
+        .then(responseJson => renderDogImages(responseJson))
+}
+
+function renderDogImages(responseJson) {
+    let results = [];
+    for (let x = 0; x < responseJson.message.length; x++) {
+        results.push(`<img src="${responseJson.message[x]}" alt="randomly generated dog image">`);
+    };
+    results.join('');
+    $('.dog-images').html(results);
 }
 
 function onPageLoad() {
