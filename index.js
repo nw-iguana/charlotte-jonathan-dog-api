8d4148b9c587591fa0c682e9edebef60bd13c5fe
Jonathan Espinosa <jonathan.espinosa@gmail.com>
2019-02-19 21:43:43 -0800

    finished step 3, need to resolve error message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function handleSubmitButton() {
+function handleDogGeneratorSubmitButton() {
     $('.dog-generator').submit(function(event) {
         event.preventDefault();
         let selection = $('.1-50').val();
@@ -38,8 +38,34 @@ function renderDogImages(responseJson) {
     $('.dog-images').html(results);
 }
 
+function handleBreedGeneratorSubmitButton() {
+    $('.breed-generator').submit(function(event) {
+        event.preventDefault();
+        let breedSelection = $('#dog-breed').val();
+        generateBreed(breedSelection);
+    })
+}
+
+function generateBreed(breedSelection) {
+    fetch(`https://dog.ceo/api/breed/${breedSelection}/images/random`)
+        .then(response => response.json())
+        .then(responseJson => renderBreedImages(responseJson))
+        .catch(error => generateError());
+}
+
+function renderBreedImages(responseJson) {
+    console.log(responseJson);
+    let html = `<img src="${responseJson.message}" alt="randomly generated dog breed image">`;
+    $('.breed-images').html(html);
+}
+
+function generateError() {
+    $('.breed-images').html(`<img src="https://httpstatusdogs.com/img/404.jpg" alt="no dogs found error">`);
+}
+
 function onPageLoad() {
-    handleSubmitButton();
+    handleDogGeneratorSubmitButton();
+    handleBreedGeneratorSubmitButton();
     generateDropDownMenu();
 }
 
